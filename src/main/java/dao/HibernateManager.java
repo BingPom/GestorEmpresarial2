18a1040945c6566ff48c972d0702e79d6285a1b8
Alvaro Serna <alvaro.srnr@gmail.com>
2023-11-16 14:08:38 +0100

    Nada especial

--- src/main/java/dao/HibernateManager.java
+++ src/main/java/dao/HibernateManager.java
@@ -9,27 +9,27 @@ import lombok.Getter;
 @Getter
 public class HibernateManager {
 	private static HibernateManager controller;
-	
+
 	private EntityManagerFactory entityManagerFactory;
 	private EntityManager manager;
 	private EntityTransaction transaction;
-	
+
 	private HibernateManager() {
 	}
-	
+
 	public static HibernateManager getInstance() {
 		if (controller == null) {
 			controller = new HibernateManager();
 		}
 		return controller;
 	}
-	
+
 	public void open() {
 		entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit");
 		manager = entityManagerFactory.createEntityManager();
 		transaction = manager.getTransaction();
 	}
-	
+
 	public void close() {
 		manager.close();
 		entityManagerFactory.close();
